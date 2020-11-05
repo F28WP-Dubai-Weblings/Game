0bce5194eb377a498a1ab203fc8d1995420e5c7f
sumaiyahshaik <71430595+sumaiyahshaik@users.noreply.github.com>
2020-11-05 09:43:11 +0400

    Update game.js

--- client/js/game.js
+++ client/js/game.js
@@ -1,10 +1,35 @@
 // js code here
-var canvas = document.getElementById("Game");
-var context = canvas.getContext("2d");
-var x = 425;
-var y = 3;
-var Width = 556;
-var Height = 556;
-var intervalVar;
+var raceGame;
+var spwanpoint=0;
+var type=0;
+var raceObstacles = [];
+var raceScore;
 
-console.log("the client script is working")
+
+function startGame() {
+    raceGame = new component(40, 70, "c1.png",200,400, "image");
+    raceGame.gravity = 0.00;
+    raceScore = new component("30px", "Impact", "white", 150, 40, "text");
+    raceGameArea.start();
+}
+
+var raceGameArea = {
+    canvas : document.getElementById("Game") ,
+        start : function() {
+        this.canvas.width = 420;
+        this.canvas.height = 500;
+		this.canvas.id="demo";
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+		 window.addEventListener('keydown', function (e) {
+            raceGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            raceGameArea.key = false;
+        })
+
+console.log("the client script is working") ;
+
+
