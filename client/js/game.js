f36e84de9095aa0fdcfbb201283544ce22a18edf
sherica123 <71396059+sherica123@users.noreply.github.com>
2020-11-09 12:29:47 +0400

    Update game.js

--- client/js/game.js
+++ client/js/game.js
@@ -48,68 +48,24 @@ function downarrow() {
       }
 
 }
-//adding obstacles
-var myGameArea = {
-  canvas : document.createElement("canvas"),
-  start : function() {
-    this.canvas.width = 480;
-    this.canvas.height = 270;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20);
-  },
-  clear : function() {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  },
-  stop : function() {
-    clearInterval(this.interval);
-  }
+//background music
+function startGame() {
+    
+    
+    myMusic = new sound("gametheme.mp3");
+    myMusic.play();
+    myGameArea.start();
 }
-
-function component(width, height, color, x, y) {
-  this.width = width;
-  this.height = height;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.x = x;
-  this.y = y;
-  this.update = function() {
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
-  this.newPos = function() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-  }
-  this.crashWith = function(otherobj) {
-    var myleft = this.x;
-    var myright = this.x + (this.width);
-    var mytop = this.y;
-    var mybottom = this.y + (this.height);
-    var otherleft = otherobj.x;
-    var otherright = otherobj.x + (otherobj.width);
-    var othertop = otherobj.y;
-    var otherbottom = otherobj.y + (otherobj.height);
-    var crash = true;
-    if ((mybottom < othertop) ||
-    (mytop > otherbottom) ||
-    (myright < otherleft) ||
-    (myleft > otherright)) {
-      crash = false;
+function updateGameArea() //this function : stops the music when the car hits an obstacle.
+{
+    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (myGamePiece.crashWith(myObstacles[i])) {
+            
+            myMusic.stop();
+            myGameArea.stop();
+            return;
+        } 
     }
-    return crash;
-  }
-}
 
-function updateGameArea() {
-  if (myGamePiece.crashWith(myObstacle)) {
-    myGameArea.stop();
-  } else {
-    myGameArea.clear();
-    myObstacle.update();
-    myGamePiece.newPos();
-    myGamePiece.update();
-  }
-}
 
