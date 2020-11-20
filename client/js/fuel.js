21513cc93a6594da666c6ba3f4abc9b9dfbd5886
Rida <raqp2000@hw.ac.uk>
2020-11-20 15:25:43 +0400

    Creating modules for Fuel and Liveplayers classes

---
+++ client/js/fuel.js
@@ -0,0 +1,16 @@
+class Fuel {
+    constructor({horizontalPos =10,verticalPos=10}) {
+      this.horizontalPos = horizontalPos;
+      this.verticalPos = verticalPos;
+      this.width = 70;
+      this.height = 40;
+    }
+  
+    draw(ctx) {
+      ctx.beginPath();
+      const img = new Image();
+          img.src = "https://f28wp-dubai-weblings.github.io/Game/client/media/fuel.png";
+          ctx.drawImage(img, this.horizontalPos, this.verticalPos, img.width, 150);
+    }
+}
+module.exports = Fuel;
--- client/js/liveplayers.js
+++ client/js/liveplayers.js
@@ -1,24 +1,49 @@
 
+
 class Liveplayers{
-    constructor({id}){
+    constructor({id, num}){
         this.id = id;
+        this.num = num;
         this.horizontalPos = 10;
         this.verticalPos = 10;
         this.score = 0;
-        this.movement = {
-            ArrowUp: false,
-            ArrowDown: false,
+        this.keyEvents = {  //set the default values of all relevant key events to false
+            ArrowUp:   false,
+            ArrowDown:  false,
             ArrowRight: false,
             ArrowLeft: false
-        };
-        this.acceleration = 10;
+        }; 
+        this.speed = 10;
     }
     
     draw(ctx){
         ctx.beginPath();
-        ctx.fillStyle = "red";
-        ctx.fillRect(this.horizontalPos, this.verticalPos, 50, 50);
+        const img = new Image();
+        if (this.num === 1){
+            img.src = "https://f28wp-dubai-weblings.github.io/Game/client/media/racecar1.png";
+        }
+        if (this.num===2){
+
+        }
+        ctx.drawImage(img, this.horizontalPos, this.verticalPos, img.width, 150);
+    }
+
+    move(){
+        if (this.keyEvents.ArrowUp) {   //move up when ArrowUp is pressed & don't let the car move above 70px (height)
+            this.verticalPos -= this.speed;
+        }
+        if (this.keyEvents.ArrowLeft) {   //move left when ArrowLeft is pressed & set minimum horizontal position as 35px (width)
+            this.horizontalPos -= this.speed;
+        }
+
+        if (this.keyEvents.ArrowDown) {  //move down when ArrowDown is pressed & don't let the car move beyond 1000px (height)
+            this.verticalPos += this.speed;
+        }
+        if (this.keyEvents.ArrowRight) {    //move right when the ArrowRight is press & don't let the car move beyond 1020px (width)
+            this.horizontalPos += this.speed;
+        }
+        this.draw(ctx);
     }
 }
 
-export default Liveplayers;
+module.exports = Liveplayers;
