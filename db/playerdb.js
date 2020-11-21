8dc3344044ba25fb1f697658a6321fb33d81c5d1
susanprog <71809941+susanprog@users.noreply.github.com>
2020-11-21 22:03:15 +0400

    Create playerdb.js

---
+++ db/playerdb.js
@@ -0,0 +1,12 @@
+//const { Player } = require('../models/entities');
+var SQL = require('sql-template-strings');
+const mysql = require('mysql');
+
+var pool = mysql.createPool({
+    connectionLimit: 100,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "mytable",
+    debug: true
+});
