a9d5e87eb5bc8dccd33f2e552f37b15e93f7dd71
susanprog <71809941+susanprog@users.noreply.github.com>
2020-11-21 12:05:30 +0400

    function call syntax changed

--- client/js/multiplayer.js
+++ client/js/multiplayer.js
@@ -48,7 +48,7 @@ function controls(player, socket) {
         event.preventDefault(); //disregrard the inbuilt default representation of the key events
         player.keyEvents[event.key] = true;
 
-        player.move();   //try without dir?
+        Liveplayers.move();   //try without dir?
         socket.emit("playerMoved", {id: player.id, horizontalPos: player.horizontalPos, verticalPos: player.verticalPos});
     }
 
@@ -56,7 +56,7 @@ function controls(player, socket) {
     function upKey(event){
         event.preventDefault();
         player.keyEvents[event.key] = false;
-        player.move();
+        Liveplayers.move();
     }
 
 };
