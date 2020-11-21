a725b27d0905ea4f6f93fa5afa32d775283023fe
susanprog <71809941+susanprog@users.noreply.github.com>
2020-11-21 17:27:52 +0400

    Update signin.js

--- routes/signin.js
+++ routes/signin.js
@@ -1,1 +1,19 @@
+const signin = function (request, response) {
+    console.log(request.body);
+    console.log(request.body.username + "'" + request.body.password);
+    //send response
+    var name = request.body.username;
+    var pass = request.body.password;
 
+
+    var user = { username: name, password: pass };
+
+    const initDB = require('../db/db');
+    //database 
+    initDB();
+
+    response.json(user);
+    response.end();
+};
+
+module.exports = signin;
