2d23f1c09809b3136e822715ff7b23b5d4da73a8
susanprog <71809941+susanprog@users.noreply.github.com>
2020-11-22 08:21:21 +0400

    Update signin.js

--- routes/signin.js
+++ routes/signin.js
@@ -11,6 +11,26 @@ const signin = function (request, response) {
     const initDB = require('../db/db');
     //database 
     initDB();
+    
+    const find = require('../db/db');
+    //database 
+    find();
+
+    const findByName = require('../db/db');
+    //database 
+    findByName(name);
+
+    const findById = require('../db/db');
+    //database 
+    findById();
+
+    const createUser = require('../db/db');
+    //database 
+    createUser(name, pass);
+
+    const deleteUser = require('../db/db');
+    //database 
+    deleteUser();
 
     response.json(user);
     response.end();
