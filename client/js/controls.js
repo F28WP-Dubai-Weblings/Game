9491ec6c1a19ce698756b5804a659c3e748efd7d
Rida <raqp2000@hw.ac.uk>
2020-11-23 03:25:48 +0400

    Further Updating the random player attacks

--- client/js/controls.js
+++ client/js/controls.js
@@ -22,8 +22,13 @@ function controls(player, socket) {
     document.getElementById("button").addEventListener("click", attack);
     function attack(){
         console.log("attack is called");
+        bulletx = player.horizontalPos;
+        bullety=player.verticalPos;
         player.attack = true;
-        socket.emit("playerAttack", {id: player.id})
+        attacks.forEach(attacc => {
+            attacc.generatePos(bulletx,bullety);
+        });
+        socket.emit("playerAttack", {id: player.id});
     }
 
 };
