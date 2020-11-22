8efb1922a2fbe08274d2cfb616b7903e674773c2
susanprog <71809941+susanprog@users.noreply.github.com>
2020-11-22 07:35:56 +0400

    Update register.js

--- client/js/register.js
+++ client/js/register.js
@@ -12,7 +12,30 @@ $(document).ready(function(){
             //to avoid the user from closing the dialog intempestively
             $(".ui-dialog-titlebar-close", ui).hide();
         },
-      
+      buttons: {
+          //define the two button login and cancel
+          "Login_Register": function() {
+              //when login is clicked, we create a user with the data , we emit the object with socket
+              //and we close the dialog
+             // user = new User($("#userName").val(), $("#Password").val());
+             var name =$("#userName").val();
+             var pass =$("#Password").val();
+             var user = {username:name, password:pass };//username is attribute
+             console.log(name + "'" + pass);
+             $.post('/register', user, function(result) { 
+               console.log(result);
+              
+           })
+             .fail(function() {
+                 console.log("error loading user");
+             });
+
+          },
+          Cancel: function() {
+              //cancel closes the dialog  
+              $(this).dialog("close");
+          }
+      },
   });
 
 });
