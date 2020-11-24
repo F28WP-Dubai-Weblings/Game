4b7552580838a78610a00b9a42aec1aa9a334d7f
sa2018 <71430595+sumaiyahshaik@users.noreply.github.com>
2020-11-24 14:18:48 +0400

    Timer

--- client/js/timer.js
+++ client/js/timer.js
@@ -1,7 +1,11 @@
 //TimerCode Here
-let t=1;
-setTimeout(function race(){
-    func(t++)
-    setTimeout(race,900000);
-} , 900000);
-console.log("This is the timer function");
+var timeleft = 90;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished";
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 90000);
