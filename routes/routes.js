2c5acd73786bd7442e4aaccbaec41180a9a8e93a
susanprog <71809941+susanprog@users.noreply.github.com>
2020-11-26 12:31:28 +0400

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,22 @@
+// module.exports = signin;
 const express = require('express');
+//const app = express();
 const usercontrol = require('../control/usercontrol');
 
 
 //define a router and create routes
 const router = express.Router();
-
 //create a route for /api/register
 router.post('/api/register', usercontrol.registerCtrl);
 
 //create a route for /api/login
 router.post('/api/login', usercontrol.loginCtrl);
 
+//create a route for /api/scoreboard
+router.post('/api/scoreboard', usercontrol.getScores);
+
+//create a route for /api/rows
+router.post('/api/rows', usercontrol.getRows);
 
 //export router
 module.exports = router;
