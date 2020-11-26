2bd6e7c6ebc1ba9d6f5a65e1ddd6afedd930250f
Rida <raqp2000@hw.ac.uk>
2020-11-26 09:24:56 +0400

    added messages and commented non function code

--- client/js/practice.js
+++ client/js/practice.js
@@ -1,3 +1,4 @@
+
 const score = document.querySelector(".scoreBoard");
 const startBoard = document.querySelector(".startNav");
 const gameScreen = document.querySelector(".practiceScreen");   
@@ -5,9 +6,10 @@ const end = document.querySelector(".end"); //game over alert
 
 let road = document.querySelector(".animateTrack")
 
-document.addEventListener("keydown", downKey);
-document.addEventListener("keyup", upKey);
-startBoard.addEventListener("click", start);
+//listen for any of the following events:
+document.addEventListener("keydown", downKey);  //when a key is pressed
+document.addEventListener("keyup", upKey);  //when a key is left
+startBoard.addEventListener("click", start);    //listen for when startboard is clciked
 
 let counter = 0;
 let coneCounter = 0;
@@ -17,7 +19,9 @@ let racer = {
     score:0
 };
 
-let fuelObj = {};   //declare empty dictionary/objects
+//declare empty dictionary/objects
+
+let fuelObj = {};   
 let coneObj = {};
 
 
@@ -28,13 +32,13 @@ let events = {  //set the default values of all relevant key events to false
     ArrowRight: false,
     ArrowLeft: false
 }; 
-
+//when a key is pressed
 function downKey(event){
     event.preventDefault(); //disregrard the inbuilt default representation of the key events
     events[event.key] = true;    
 }
-
-function upKey(event){
+//when a key is left
+function upKey(event){  
     event.preventDefault();
     events[event.key] = false;
 
@@ -60,9 +64,16 @@ function gameOver(){    //call when player has collided with a cone
     end.classList.remove("hide");    //add classList "hide" to get rid of the start navigation pop-up
 }
 
+
+/*flag will be true if the player has collided with a fuel and the position must me set to (1000,1000) 
+and false in the default case when a random position within the track has to be generated*/
 let flag = false;   
+
 let fuel;
 function updatePos(flag){
+
+    /*the position of the fuel must only be updated when counter value is greater than 650,
+    settimeinterval is not used as this is not exactly frame based and any errors must be avoided*/
     if (!flag){
         fuel = document.querySelector(".fuelDiv");
         counter++; 
@@ -81,12 +92,11 @@ function updatePos(flag){
     }
     fuel.style.left = fuelObj.horizontalPos + "px";
     fuel.style.top = fuelObj.verticalPos + "px";
-}{}
+}
 
 
 
-let cone;
-let bug = false;
+let cone;   //declare cone as a variable
 function updateCone() {
     cone = document.querySelector(".coneDiv");
     car = document.querySelector(".carVector");
@@ -94,19 +104,20 @@ function updateCone() {
 
     coneCounter++;
 
-    if (coneCounter >100 && coneCounter < 700){
-        coneCounter++;
+    /*the position of the cone must only be updated when concounter value is greater than 650,
+    settimeinterval is not used as this is not exactly frame based and any errors must be avoided*/
+    if (coneCounter >100 && coneCounter < 680){
+        coneCounter++;  
 
     }   
     if (coneCounter >650){
         coneObj.horizontalPos = (Math.random() * 670);
         coneObj.verticalPos = (Math.random() * 620);
-        bug = collision(car,cone);
         
         coneCounter = 0;
     } 
     
-
+    //update the position of the cones
     cone.style.left = coneObj.horizontalPos + "px";
     cone.style.top = coneObj.verticalPos + "px";
 
@@ -118,41 +129,43 @@ function move(){
     let fuel = document.querySelector(".fuelDiv");
     let cone = document.querySelector(".coneDiv");
 
-   
 
-    if(racer.ready){
+    if(racer.ready){   
 
         updatePos(false);
         updateCone();
 
         let collide = collision(car,fuel);
         if (collide){
-            racer.score +=100;
-            updatePos(true);
-            collide = false;         
+            racer.score +=100;  //increase the player score
+            updatePos(true);    //this causes the fuel position to be instantly updated to (1000,1000) so it overflows from the track and is not visible
+            collide = false;    //reset collide flag
         }
 
+        //call the game over func if player has hit a cone
         let crash = collision(car, cone);
         if (crash){
             gameOver();     
         }
 
-        if (events.ArrowUp && racer.verticalPos>0) {   //move up when ArrowUp is pressed & don't let the car move above 70px (height)
+        if (events.ArrowUp && racer.verticalPos>0) {   //move up when ArrowUp is pressed & don't let the car move above the border of the track (height)
             racer.verticalPos -= racer.pixelPosition;
         }
-        if (events.ArrowLeft && racer.horizontalPos>0) {   //move left when ArrowLeft is pressed & set minimum horizontal position as 35px (width)
+        if (events.ArrowLeft && racer.horizontalPos>0) {   //move left when ArrowLeft is pressed & dont let the car move beyond the left border of the track (width)
             racer.horizontalPos -= racer.pixelPosition;
         }
 
-        if (events.ArrowDown  && racer.verticalPos<530) {  //move down when ArrowDown is pressed & don't let the car move beyond 1000px (height)
+        if (events.ArrowDown  && racer.verticalPos<530) {  //move down when ArrowDown is pressed & don't let the car move beyond the bottom of the track (height)
             racer.verticalPos += racer.pixelPosition;
         }
-        if (events.ArrowRight && racer.horizontalPos<690) {    //move right when the ArrowRight is press & don't let the car move beyond 1020px (width)
+        if (events.ArrowRight && racer.horizontalPos<690) {    //move right when the ArrowRight is press & don't let the car move beyond the right border of the track (width)
             racer.horizontalPos += racer.pixelPosition;
         }     
         
-        racer.score+=1;
-        car.style.left = racer.horizontalPos + "px";
+        racer.score+=1;//increment the players score by 1 in every fram
+
+        //update .carVector div positon i.e., player 
+        car.style.left = racer.horizontalPos + "px";    
         car.style.top = racer.verticalPos + "px";
 
     //generate random fuel positions.
@@ -167,13 +180,13 @@ function move(){
 
 function start(){
 
-    racer.ready = true;
-    racer.score = 0;
+    racer.ready = true; //set flag as true to continue movement
+    racer.score = 0;    //reset racer's score
     
     startBoard.classList.add("hide");    //add classList "hide" to get rid of the start navigation pop-up
     gameScreen.classList.remove("hide");    //remove class list "hide" from gameScreen to make the game screen visible
     
-    window.requestAnimationFrame(move)  //invoke another play()
+    window.requestAnimationFrame(move)  //invoke another move() to start the game
 
     let car = document.createElement("div"); //create a div element in the dom
     car.setAttribute("class", "carVector"); //set class attribute of the div
@@ -187,12 +200,16 @@ function start(){
     fuel.setAttribute("class", "fuelDiv");     
     road.appendChild(fuel);    //add fuelImg to the fuelDiv
     
-    
+    //set the default positions of the fuel , these will be update in updated in updatePos() at every frame
+    fuelObj.horizontalPos = 1000;   
+    fuelObj.verticalPos = 1000;
+
     //set up cone obstacles
     let cone = document.createElement("div");
     cone.setAttribute("class", "coneDiv");
     road.appendChild(cone);
 
+    //set the default positions of the fuel , these will be update in updated in updatePos() at every frame
     coneObj.horizontalPos = 1000;
     coneObj.verticalPos = 1000;
 
